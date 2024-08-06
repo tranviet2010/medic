8dfae75364524da2a06755339579afbc5c7051d5
giangcm <rockieman777@gmail.com>
2024-08-07 02:25:10 +0700

    fix

--- src/modules/upload/schemas/upload.schemas.ts
+++ src/modules/upload/schemas/upload.schemas.ts
@@ -4,12 +4,14 @@ import { Document } from 'mongoose';
 @Schema({ timestamps: true, collection: 'files' })
 @Schema()
 export class Upload {
+  @Prop({ required: false })
+  customeId: string;
 
   @Prop({ required: true })
-  filename: string | any;
+  filename: string;
 
   @Prop({ required: true })
-  path: string | any;
+  path: string;
 }
 
 export type UploadDocument = Upload & Document;
